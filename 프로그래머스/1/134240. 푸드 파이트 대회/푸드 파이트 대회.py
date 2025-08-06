7f20dc510a46f5e6c57e21cc2c3a71efab537142
jihyunni <113456378+jihyunni@users.noreply.github.com>
2025-08-06 20:39:46 +0900

    Update 푸드 파이트 대회.py

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -3,7 +3,7 @@ def solution(food):
     
     for i in range(0, len(food)):
         
-        # food[0]은 수웅이가 준비한 물의 양이며, 항상 1입니다.
+        # 조건: food[0]은 수웅이가 준비한 물의 양이며, 항상 1입니다.
         if i == 0 :
             food[i] = 0
             answer += str(food[i])
@@ -17,3 +17,18 @@ def solution(food):
     revers = answer[:-1][::-1]
     answer += revers
     return answer
+
+---------------------------------------------------------------------------
+## GPT씨의 내 코드 보완 ##
+def solution(food):
+    answer = ''
+    
+    for i in range(0, len(food)):
+        if i == 0:
+            continue  # 굳이 0을 넣었다 뺐다 하지 말고 skip
+        else:
+            fd = food[i] // 2
+            answer += str(i) * fd
+
+    revers = answer[::-1]
+    return answer + '0' + revers
